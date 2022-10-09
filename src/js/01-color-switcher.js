1968f006e38e9336a90f7688ef25e521541c703c
YurGoV <yurgov@gmail.com>
2022-10-09 17:26:43 +0300

    task-1_draft

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,63 @@
 
+const refButtonStart = document.querySelector('button[data-start]');
+const refButtonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+console.log(refButtonStart);
+console.log(refButtonStop);
+console.log(body);
+
+// changeColorStatus = NaN;
+// switchButtonsActivity(false)
+
+refButtonStop.disabled = true;
+// refButtonStart.disabled = false;
+// const changeColorStatus = false;
+// setButtonsActivity()
+
+refButtonStart.addEventListener('click', onButtonStart);
+refButtonStop.addEventListener('click', onButtonStop);
+
+function onButtonStart(event) {
+    chhangeBackgroundColor()
+    intervalId = setInterval(chhangeBackgroundColor, 1000);
+    switchButtonsActivity(true)
+    // body.style.backgroundColor = getRandomHexColor();
+    // refButtonStop.disabled = false;
+    // refButtonStart.disabled = true;
+};
+
+function onButtonStop() {
+    console.log('ddddd');
+    clearInterval(intervalId);
+    changeColorStatus = true;
+    // chhangeBackgroundColor()
+    // setInterval(chhangeBackgroundColor, 1000)
+    // body.style.backgroundColor = getRandomHexColor();
+    // refButtonStop.disabled = true;
+    // refButtonStart.disabled = false;
+    switchButtonsActivity(false);
+};
+
+function switchButtonsActivity(changeColorStatus) {
+    // if (changeColorStatus) {
+    //     // refButtonStop.setAttribute('disabled', true);
+    //     console.log('sss');
+    //     refButtonStop.disabled = false;
+    //     refButtonStart.disabled = true;
+    //     return;
+    // }
+    // refButtonStop.disabled = true;
+    // refButtonStart.disabled = false;
+    refButtonStop.disabled = !refButtonStop.disabled;
+    refButtonStart.disabled = !refButtonStart.disabled;
+    // console.log(!refButtonStart.disabled);
+    // console.log(refButtonStop.disabled);
+}
+
+function chhangeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
